f5d5f10a00fc53371e54c6b138978070cb3b430b
taniyow <markcmtan@gmail.com>
2023-01-05 00:25:59 +0800

    Testing with Mocha

--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -4,30 +4,3 @@ const ganache = require('ganache-cli');
 const Web3 = require('web3');
 
 const web3 = new Web3(ganache.provider());
-
-class Car {
-    park() {
-        return 'stopped';
-    }
-
-    drive() {
-        return 'vroom';
-    }
-}
-
-let car;
-
-beforeEach(() => {
-    car = new Car();
-});
-
-describe('Car', () => {
-
-    it('can park', () => {
-        assert.strictEqual(car.park(), 'stopped');
-    });
-
-    it('can drive', () => {
-        assert.strictEqual(car.drive(), 'vroom');
-    });
-});
