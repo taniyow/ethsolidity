612efd6756c756ec73a85bcc09ea953563297ec8
taniyow <markcmtan@gmail.com>
2023-01-05 00:16:34 +0800

    Testing with Mocha Sample Test Code Block

--- inbox/compile.js
+++ inbox/compile.js
@@ -1,1 +1,14 @@
 // compile code will go here
+const path = require('path');
+const fs = require('fs');
+
+const solc = require('solc');
+
+// path to look the sol file
+const  inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
+
+// read the sol file
+const source = fs.readFileSync(inboxPath, 'utf8');
+
+console.log(solc.compile(source, 1));
+module.exports = solc.compile(source, 1).contracts[':Inbox'];
