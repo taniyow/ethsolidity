e708124cc505f674e6a2d76c278376ae631ca2b5
taniyow <markcmtan@gmail.com>
2023-01-05 11:07:11 +0800

    Refactor assert.equal to assert.strictEqual in Testing Scripts

--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -26,12 +26,12 @@ describe('Inbox', () => {
 
     it('has a default message', async () => {
         const message = await inbox.methods.message().call();
-        assert.equal(message, 'Hello World!');
+        assert.strictEqual(message, 'Hello World!');
     });
 
     it('can change the message', async () => {
         await inbox.methods.setMessage('Bye').send({ from: accounts[0] });
         const message = await inbox.methods.message().call();
-        assert.equal(message, 'Bye');
+        assert.strictEqual(message, 'Bye');
     });
 });
