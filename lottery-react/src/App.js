1a03b9f88e941c4780ac970d5ae6e5853298a273
taniyow <markcmtan@gmail.com>
2023-01-26 01:09:03 +0800

    Setting up the Form

--- lottery-react/src/App.js
+++ lottery-react/src/App.js
@@ -1,27 +1,60 @@
 import React, { Component } from "react";
 import "./App.css";
-import lottery from './lottery';
+import lottery from "./lottery";
+import web3 from "./web3";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
+    state = {
+        manager: '',
+        players: [],
+        balance: '',
+        value: '',
+    };
 
-    this.state = { manager: '' };
-  }
+    async componentDidMount() {
+        const manager = await lottery.methods.manager().call();
+        const players = await lottery.methods.getPlayers().call();
+        const balance = await web3.eth.getBalance(lottery.options.address);
 
-  async componentDidMount() {
-    const manager = await lottery.methods.manager().call();
-  
-    this.setState({ manager });
-  }
+        this.setState({ manager, players, balance });
+    }
 
-  render() {
-    return (
-      <div>
-        <h2>Lottery Contract</h2>
-        <p>This contract is managed by {this.state.manager}</p>
-      </div>
-    );
-  }
+    onSubmit = async (event) => {
+        event.preventDefault();
+
+        const accounts = await web3.eth.getAccounts();
+
+        await lottery.methods.enter().send({
+            from: accounts[0],
+            value: web3.utils.toWei(this.state.value, 'ether')
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>Lottery Contract</h2>
+                <p>
+                    This contract is managed by {this.state.manager}. There are
+                    currently {this.state.players} people entered, competing to
+                    win {web3.utils.fromWei(this.state.balance, "ether")} ether!
+                </p>
+
+                <hr/>
+
+                <form onSubmit={this.onSubmit}>
+                    <h4>Want to try your luck?</h4>
+                    <div>
+                        <label>Amount of ether to enter </label>
+                        <input
+                            value={this.state.value}
+                            onChange={event => this.setState({ value: event.target.value })}
+                        />
+                    </div>
+                    <button>Enter</button>
+                </form>
+            </div>
+        );
+    }
 }
 export default App;
