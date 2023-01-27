2bdfd80354fdeb3fddd060ad1914e7eab93daaf4
taniyow <markcmtan@gmail.com>
2023-01-27 22:22:31 +0800

    Adding Function for Picking Winner

--- lottery-react/src/App.js
+++ lottery-react/src/App.js
@@ -35,6 +35,18 @@ class App extends Component {
         this.setState({ message: 'You have entered!' });
     }
 
+    onClick = async () => {
+        const accounts = await web3.eth.getAccounts();
+        
+        this.setState({ message: "Waiting on transaction success..." });
+
+        await lottery.methods.pickWinner().send({
+            from: accounts[0],
+        });
+
+        this.setState({ message: 'A winner has been picked!' });
+    }
+
     render() {
         return (
             <div>
@@ -58,6 +70,11 @@ class App extends Component {
                     </div>
                     <button>Enter</button>
                 </form>
+
+                <hr />
+
+                <h4>Ready to pick a winner?</h4>
+                <button onClick={this.onClick}>Pick a Winner</button>
 
                 <hr />
 
