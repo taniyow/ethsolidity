ae66f69c2a49636cb23071c56952e633be9e2c85
taniyow <markcmtan@gmail.com>
2023-01-31 00:50:08 +0800

    Configuring test and creating instances of the two contracts

---
+++ kickstart/test/Campaign.test.js
@@ -0,0 +1,39 @@
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
+const web3 = new Web3(ganache.provider());
+
+const compiledFactory = require('../ethereum/build/CampaignFactory.json');
+const compiledCampaign = require('../ethereum/build/Campaign.json');
+
+let accounts;
+let factory;
+let campaignAddress;
+let campaign;
+
+beforeEach(async () => {
+    accounts = await web3.eth.getAccounts();
+
+    // let factory create an instance of campaign factory contract
+    factory = await new web3.eth.Contract(JSON.parse(compiledFactory.interface))
+        .deploy({ data: compiledFactory.bytecode })
+        .send({ from: accounts[0], gas: '1000000' });
+
+    // let this to test the createCampaign function
+    await factory.methods.createCampaign('100').send({
+        from: accounts[0],
+        gas: '1000000'
+    });
+
+    // let this to test the getDeployedCampaigns function
+    // [campaignAddress] is a es6 syntax and is similar to
+    // const addresses = await factory.methods...
+    // campaignAddress = addresses[0]
+    [campaignAddress] = await factory.methods.getDeployedCampaigns().call();
+
+    // let campaign create an instance of campaign contract
+    campaign = await new web3.eth.Contract(
+        JSON.parse(compiledCampaign.interface),
+        campaignAddress
+    );
+});
