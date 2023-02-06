259ef7227683a79efa2beae0cd7b5894b2218fcd
taniyow <markcmtan@gmail.com>
2023-02-06 14:01:08 +0800

    Kickstart Deployment - Refactoring some deployjs script

---
+++ kickstart/ethereum/deploy.js
@@ -0,0 +1,25 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+const compiledFactory = require('./build/CampaignFactory.json');
+
+const provider = new HDWalletProvider(
+  'ridge gown oyster provide bar evolve puppy derive grant work barrel omit',
+  'https://goerli.infura.io/v3/5bd60364dd204c3d881cb312f1b0d783',
+);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log('Attempting to deploy from account', accounts[0]);
+
+  const result = await new web3.eth.Contract(
+    JSON.parse(compiledFactory.interface)
+  )
+    .deploy({ data: bytecode })
+    .send({ gas: '1000000', from: accounts[0] });
+
+  console.log('Contract deployed to', result.options.address);
+  provider.engine.stop();
+};
+deploy();
