4e6639eb579e270a4b586478aa13cde91ecf69dc
taniyow <markcmtan@gmail.com>
2023-02-06 23:49:32 +0800

    Adding SemanticUI Card Group to display the campaigns

--- kickstart/pages/index.js
+++ kickstart/pages/index.js
@@ -1,7 +1,8 @@
-import 'semantic-ui-css/semantic.min.css'
-
 import { Component } from 'react';
+import 'semantic-ui-css/semantic.min.css';
+
 import factory from '../ethereum/factory';
+import { Card } from 'semantic-ui-react';
 
 class CampaignIndex extends Component {
     static async getInitialProps() {
@@ -10,8 +11,20 @@ class CampaignIndex extends Component {
         return { campaigns };
     }
 
+    renderCampaigns() {
+        const items = this.props.campaigns.map(address => {
+            return {
+                header: address,
+                description: <a>View Campaign</a>,
+                fluid: true
+            };
+        });
+
+        return <Card.Group items={items} />;
+    }
+
     render() {
-        return <div>{this.props.campaigns[0]}</div>
+        return <div>{this.renderCampaigns()}</div>
     }
 };
 
