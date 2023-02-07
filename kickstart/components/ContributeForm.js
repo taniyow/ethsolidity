762f11240f5cc718d56164a50735f9be6dd3bd1f
taniyow <markcmtan@gmail.com>
2023-02-08 00:51:21 +0800

    Adding Button spinners and handling error messages

--- kickstart/components/ContributeForm.js
+++ kickstart/components/ContributeForm.js
@@ -8,13 +8,17 @@ import { Router } from '../routes';
 
 class ContributeForm extends Component {
     state = {
-        value: ''
+        value: '',
+        errorMessage: '',
+        loading: false
     }
 
     onSubmit = async (event) => {
         event.preventDefault();
 
         const campaign = new Campaign(this.props.address);
+
+        this.setState({ loading: true, errorMessage: '' });
 
         try {
             const accounts = await web3.eth.getAccounts();
@@ -25,13 +29,15 @@ class ContributeForm extends Component {
 
             Router.replaceRoute(`/campaigns/${this.props.address}`)
         } catch (err) {
-
+            this.setState({ errorMessage: err.message})
         }
+
+        this.setState({ loading: false, value: '' });
     }
 
     render() {
         return (
-            <Form onSubmit={this.onSubmit}>
+            <Form onSubmit={this.onSubmit} error={!!this.state.errorMessage}>
                 <Form.Field>
                     <label>Amount to Contribute</label>
                     <Input
@@ -43,7 +49,9 @@ class ContributeForm extends Component {
                         labelPosition="right"
                     />
                 </Form.Field>
-                <Button primary>
+
+                <Message error header="Oops!" content={this.state.errorMessage} />
+                <Button primary loading={this.state.loading}>
                     Contribute
                 </Button>
             </Form>
