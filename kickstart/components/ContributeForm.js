fa373bf3b05a20dd5ccd69d4e583e65a509c84a5
taniyow <markcmtan@gmail.com>
2023-02-08 00:03:34 +0800

    Adding state to Contribute Form

--- kickstart/components/ContributeForm.js
+++ kickstart/components/ContributeForm.js
@@ -2,13 +2,25 @@ import { Component } from 'react';
 import { Form, Input, Button, Message } from 'semantic-ui-react';
 
 class ContributeForm extends Component {
+    state = {
+        value: ''
+    }
+
+    onSubmit = (event) => {
+        event.preventDefault();
+        
+    }
 
     render() {
         return (
-            <Form>
+            <Form onSubmit={this.onSubmit}>
                 <Form.Field>
                     <label>Amount to Contribute</label>
                     <Input
+                        value={this.state.value}
+                        onChange={event => 
+                            this.setState({ value: event.target.value })
+                        }
                         label="ether"
                         labelPosition="right"
                     />
