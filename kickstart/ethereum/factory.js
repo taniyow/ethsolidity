b32ace03855aaa8a3056d8789cb292b90524c954
taniyow <markcmtan@gmail.com>
2023-02-07 22:25:07 +0800

    Redeploying CampaignFactory and getting its new address

--- kickstart/ethereum/factory.js
+++ kickstart/ethereum/factory.js
@@ -3,7 +3,7 @@ import CampaignFactory from './build/CampaignFactory.json';
 
 const instance = new web3.eth.Contract(
     JSON.parse(CampaignFactory.interface),
-    '0x5E6fa912B8686b6C16A6B401Af8183D8799BFcFA'
+    '0xBC593751EC0b39003Beb419044547fe6cce51746'
 );
 
 export default instance;
