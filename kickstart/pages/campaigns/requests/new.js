9d557b387ef698c2ce2c59f0a808ee8d3e65c33e
taniyow <markcmtan@gmail.com>
2023-02-08 02:24:04 +0800

    Adding routing to requests and new request

---
+++ kickstart/pages/campaigns/requests/new.js
@@ -0,0 +1,14 @@
+import { Component } from 'react';
+import Layout from '../../../components/Layout';
+
+class RequestNew extends Component {
+    render(){
+        return (
+            <Layout>
+                <h3>Create a Request</h3>
+            </Layout>
+        )
+    }
+}
+
+export default RequestNew;
