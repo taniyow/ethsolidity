8d4719be0b1eb9abcbbe070da8429b979be71a33
taniyow <markcmtan@gmail.com>
2023-02-09 23:48:17 +0800

    Used JS Array Mapping to iterate the campaign methods lists by index and requestCount

--- kickstart/pages/campaigns/requests/index.js
+++ kickstart/pages/campaigns/requests/index.js
@@ -2,12 +2,21 @@ import { Component } from 'react';
 import { Button } from 'semantic-ui-react';
 import { Link } from '../../../routes';
 import Layout from '../../../components/Layout';
+import Campaign from '../../../ethereum/campaign';
 
 class RequestIndex extends Component {
     static async getInitialProps(props) {
         const { address } = props.query;
+        const campaign = Campaign(address);
+        const requestCount = await campaign.methods.getRequestsCount().call();
 
-        return { address }
+        const requests = await Promise.all(
+            Array(parseInt(requestCount)).fill().map((element, index) => {
+                return campaign.methods.requests(index).call()
+            })
+        )
+
+        return { address, requests, requestCount }
     }
 
     render() {
