79e94d3e1aa7f6c9c1da98604404cc5c852b2b42
taniyow <markcmtan@gmail.com>
2023-02-10 00:39:21 +0800

    Render contents of individual cells of each requests row

--- kickstart/pages/campaigns/requests/index.js
+++ kickstart/pages/campaigns/requests/index.js
@@ -10,6 +10,7 @@ class RequestIndex extends Component {
         const { address } = props.query;
         const campaign = Campaign(address);
         const requestCount = await campaign.methods.getRequestsCount().call();
+        const approversCount = await campaign.methods.approversCount().call();
 
         const requests = await Promise.all(
             Array(parseInt(requestCount)).fill().map((element, index) => {
@@ -17,16 +18,20 @@ class RequestIndex extends Component {
             })
         )
 
-        return { address, requests, requestCount }
+        return { address, requests, requestCount, approversCount };
     }
 
     renderRows() {
         return this.props.requests.map((request, index) => {
-            return <RequestRow 
-                key={index}
-                request={request}
-                address={this.props.address}
-            />
+            return (
+                <RequestRow 
+                    key={index}
+                    id={index}
+                    request={request}
+                    address={this.props.address}
+                    approversCount={this.props.approversCount}
+                />
+            )
         })
     }
 
