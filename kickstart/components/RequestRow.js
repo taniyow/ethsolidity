b0060bb060c4a66a03f8dec91205e261f3acf50f
taniyow <markcmtan@gmail.com>
2023-02-10 00:01:43 +0800

    Creating new component to render each row

---
+++ kickstart/components/RequestRow.js
@@ -0,0 +1,11 @@
+import { Component } from 'react';
+
+class RequestRow extends Component {
+    render() {
+        return (
+            <div>Request</div>
+        )
+    }
+}
+
+export default RequestRow;
