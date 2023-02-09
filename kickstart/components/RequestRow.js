2a87f256d7dc1190801b41ff9727b198898b927b
taniyow <markcmtan@gmail.com>
2023-02-10 01:39:41 +0800

    Styled Buttons and Color indicator if its ready for approval or finalization

--- kickstart/components/RequestRow.js
+++ kickstart/components/RequestRow.js
@@ -28,22 +28,28 @@ class RequestRow extends Component {
         // used to destructure
         const { id, request, approversCount } = this.props;
 
+        const readToFinalize = request.approvalCount > approversCount / 2;
+
         return (
-            <Row>
+            <Row disabled={request.complete} positive={readToFinalize && !request.complete}>
                 <Cell>{id}</Cell>
                 <Cell>{request.description}</Cell>
                 <Cell>{web3.utils.fromWei(request.value, 'ether')}</Cell>
                 <Cell>{request.recipient}</Cell>
                 <Cell>{request.approvalCount}/{approversCount}</Cell>
                 <Cell>
-                    <Button color="green" basic onClick={this.onApprove}>
-                        Approve
-                    </Button>
+                    {request.complete ? null : (
+                        <Button color="green" basic onClick={this.onApprove}>
+                            Approve
+                        </Button>
+                    )}
                 </Cell>
                 <Cell>
-                    <Button color="teal" basic onClick={this.onFinalize}>
-                        Finalize
-                    </Button>
+                    {request.complete ? null : (
+                        <Button color="teal" basic onClick={this.onFinalize}>
+                            Finalize
+                        </Button>
+                    )}
                 </Cell>
             </Row>
         )
